4fe14ad98e715dd4e6786bbc447ac4f4cc83e156
Xiem Ngo <xiemnt05.ssc@gmail.com>
2021-10-17 12:00:22 +0700

    add responsive

--- app.js
+++ app.js
@@ -7,3 +7,19 @@ window.addEventListener("scroll", (e) => {
     header.classList.remove("sticky");
   }
 });
+
+const projects = document.querySelectorAll(".project");
+const projectImages = document.querySelectorAll(".project img");
+projects.forEach((project) => {
+  project.addEventListener("mouseover", (e) => {
+    e.currentTarget.firstElementChild.style.transform = "scale(1.1)";
+    e.currentTarget.firstElementChild.style.transition = "all 0.3s ease-in-out";
+  });
+});
+
+projects.forEach((project) => {
+  project.addEventListener("mouseleave", (e) => {
+    e.currentTarget.firstElementChild.style.transform = "scale(1)";
+    e.currentTarget.firstElementChild.style.transition = "all 0.3s ease-in-out";
+  });
+});
